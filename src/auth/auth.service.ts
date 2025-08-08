f34d1b42fd4d245fb0b3be82cbaa8af2c67622ff
Rupee88 <chyrupesh828@gmail.com>
2025-08-08 14:22:53 +0545

    Added Bcryptjs for Hash Password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-
+import bcryt from 'bcrypt';
 @Injectable()
 export class AuthService {
   async register(createUserDto: CreateUserDto) {
     // Logic for user registration
+
+    // For example, hashing the password before saving to the database
+    const hashedPassword = await bcryt.hash(createUserDto.password, 10);
+    createUserDto.password = hashedPassword;
     return createUserDto;
   }
 }
