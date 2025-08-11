01c27ace78c198507eb93994b3bc60bfed0b5c47
Rupee88 <chyrupesh828@gmail.com>
2025-08-11 14:26:54 +0545

    Added changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,13 @@ import bcryt from 'bcrypt';
 @Injectable()
 export class AuthService {
   async register(createUserDto: CreateUserDto) {
-    // Logic for user registration
+   
 
     // For example, hashing the password before saving to the database
     const hashedPassword = await bcryt.hash(createUserDto.password, 10);
     createUserDto.password = hashedPassword;
+
+    //save user in the databse
     return createUserDto;
   }
 }
